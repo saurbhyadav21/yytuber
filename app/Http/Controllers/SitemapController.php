78bc92eac60ba7b72ca7a6ff978cfe32ba25e890
saurbhcoder <saurbhcoder@gmail.com>
2026-06-23 11:08:34 +0530

    sitemap

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -2,15 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Response;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
 
 class SitemapController extends Controller
 {
     public function index()
     {
-        $content = view('sitemap')->render();
+        $sitemap = Sitemap::create()
 
-        return response($content, 200)
+            ->add(
+                Url::create('/')
+                    ->setPriority(1.0)
+            )
+
+            ->add(
+                Url::create('/youtube-thumbnail-downloader')
+                    ->setPriority(0.9)
+            )
+
+            ->add(
+                Url::create('/youtube-tag-extractor')
+                    ->setPriority(0.9)
+            );
+
+        return response($sitemap->render())
             ->header('Content-Type', 'application/xml');
     }
 }
