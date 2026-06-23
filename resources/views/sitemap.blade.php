e538b3e1844b15849a39ad3a92eb695d2697ed51
saurbhcoder <saurbhcoder@gmail.com>
2026-06-23 11:10:35 +0530

    siemap

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -2,22 +2,12 @@
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
-    <url>
-        <loc>https://yytuber.com/</loc>
-        <changefreq>daily</changefreq>
-        <priority>1.0</priority>
-    </url>
-
-    <url>
-        <loc>https://yytuber.com/youtube-thumbnail-downloader</loc>
-        <changefreq>weekly</changefreq>
-        <priority>0.9</priority>
-    </url>
-
-    <url>
-        <loc>https://yytuber.com/youtube-tag-extractor</loc>
-        <changefreq>weekly</changefreq>
-        <priority>0.9</priority>
-    </url>
+@foreach($pages as $page)
+<url>
+    <loc>{{ url($page) }}</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.8</priority>
+</url>
+@endforeach
 
 </urlset>
