c59236fbd455d7e73548992616f27fe30a35f01b
saurbhcoder <saurbhcoder@gmail.com>
2026-06-24 11:15:49 +0530

    x

---
+++ config/breadcrumbs.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+
+'home'=>[
+['name'=>'Home','url'=>url('/')]
+],
+
+'about'=>[
+['name'=>'Home','url'=>url('/')],
+['name'=>'About','url'=>url('/about')]
+],
+
+'contact'=>[
+['name'=>'Home','url'=>url('/')],
+['name'=>'Contact','url'=>url('/contact')]
+],
+
+'privacy'=>[
+['name'=>'Home','url'=>url('/')],
+['name'=>'Privacy Policy','url'=>url('/privacy')]
+],
+
+'terms'=>[
+['name'=>'Home','url'=>url('/')],
+['name'=>'Terms of Use','url'=>url('/terms')]
+],
+
+'youtube-thumbnail'=>[
+['name'=>'Home','url'=>url('/')],
+['name'=>'Tools','url'=>url('/')],
+['name'=>'YouTube Thumbnail Downloader','url'=>url('/youtube-thumbnail')]
+],
+
+'profile-pictures'=>[
+['name'=>'Home','url'=>url('/')],
+['name'=>'Tools','url'=>url('/')],
+['name'=>'Profile Pictures Download','url'=>url('/profile-pictures')]
+],
+
+'banner-download'=>[
+['name'=>'Home','url'=>url('/')],
+['name'=>'Tools','url'=>url('/')],
+['name'=>'Banner Download','url'=>url('/banner-download')]
+],
+
+'monetization-checker'=>[
+['name'=>'Home','url'=>url('/')],
+['name'=>'Tools','url'=>url('/')],
+['name'=>'YouTube Monetization Checker','url'=>url('/monetization-checker')]
+],
+
+];
