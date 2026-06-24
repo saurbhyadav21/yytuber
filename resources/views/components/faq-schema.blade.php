ea487d02eadca9ab24e1110d906c61de5621259e
saurbhcoder <saurbhcoder@gmail.com>
2026-06-24 09:46:58 +0530

    faq

---
+++ resources/views/components/faq-schema.blade.php
@@ -0,0 +1,18 @@
+<script type="application/ld+json">
+{
+    "@context": "https://schema.org",
+    "@type": "FAQPage",
+    "mainEntity": [
+        @foreach($faqs as $faq)
+        {
+            "@type": "Question",
+            "name": @json($faq['question']),
+            "acceptedAnswer": {
+                "@type": "Answer",
+                "text": @json($faq['answer'])
+            }
+        }@if(!$loop->last),@endif
+        @endforeach
+    ]
+}
+</script>
