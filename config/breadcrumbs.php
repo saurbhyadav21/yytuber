25cd1950d124f89f77932c3e4b4e200d36e14aa3
saurbhcoder <saurbhcoder@gmail.com>
2026-06-24 11:26:09 +0530

    x

--- config/breadcrumbs.php
+++ config/breadcrumbs.php
@@ -2,52 +2,52 @@
 
 return [
 
-'home'=>[
-['name'=>'Home','url'=>url('/')]
-],
+    'home' => [
+        ['name' => 'Home', 'url' => url('/')]
+    ],
 
-'about'=>[
-['name'=>'Home','url'=>url('/')],
-['name'=>'About','url'=>url('/about')]
-],
+    'about' => [
+        ['name' => 'Home', 'url' => url('/')],
+        ['name' => 'About', 'url' => url('/about')]
+    ],
 
-'contact'=>[
-['name'=>'Home','url'=>url('/')],
-['name'=>'Contact','url'=>url('/contact')]
-],
+    'contact' => [
+        ['name' => 'Home', 'url' => url('/')],
+        ['name' => 'Contact', 'url' => url('/contact')]
+    ],
 
-'privacy'=>[
-['name'=>'Home','url'=>url('/')],
-['name'=>'Privacy Policy','url'=>url('/privacy')]
-],
+    'privacy' => [
+        ['name' => 'Home', 'url' => url('/')],
+        ['name' => 'Privacy Policy', 'url' => url('/privacy')]
+    ],
 
-'terms'=>[
-['name'=>'Home','url'=>url('/')],
-['name'=>'Terms of Use','url'=>url('/terms')]
-],
+    'terms' => [
+        ['name' => 'Home', 'url' => url('/')],
+        ['name' => 'Terms of Use', 'url' => url('/terms')]
+    ],
 
-'youtube-thumbnail'=>[
-['name'=>'Home','url'=>url('/')],
-['name'=>'Tools','url'=>url('/')],
-['name'=>'YouTube Thumbnail Downloader','url'=>url('/youtube-thumbnail')]
-],
+    'youtube-thumbnail' => [
+        ['name' => 'Home', 'url' => url('/')],
+        ['name' => 'Tools', 'url' => url('/')],
+        ['name' => 'YouTube Thumbnail Downloader', 'url' => url('/youtube-thumbnail')]
+    ],
 
-'profile-pictures'=>[
-['name'=>'Home','url'=>url('/')],
-['name'=>'Tools','url'=>url('/')],
-['name'=>'Profile Pictures Download','url'=>url('/profile-pictures')]
-],
+    'profile-pictures' => [
+        ['name' => 'Home', 'url' => url('/')],
+        ['name' => 'Tools', 'url' => url('/')],
+        ['name' => 'Profile Pictures Download', 'url' => url('/profile-pictures')]
+    ],
 
-'banner-download'=>[
-['name'=>'Home','url'=>url('/')],
-['name'=>'Tools','url'=>url('/')],
-['name'=>'Banner Download','url'=>url('/banner-download')]
-],
+    'banner-download' => [
+        ['name' => 'Home', 'url' => url('/')],
+        ['name' => 'Tools', 'url' => url('/')],
+        ['name' => 'Banner Download', 'url' => url('/banner-download')]
+    ],
 
-'monetization-checker'=>[
-['name'=>'Home','url'=>url('/')],
-['name'=>'Tools','url'=>url('/')],
-['name'=>'YouTube Monetization Checker','url'=>url('/monetization-checker')]
-],
+    'monetization-checker' => [
+        ['name' => 'Home', 'url' => url('/')],
+        ['name' => 'Tools', 'url' => url('/')],
+        ['name' => 'YouTube Monetization Checker', 'url' => url('/monetization-checker')]
+    ],
 
 ];
