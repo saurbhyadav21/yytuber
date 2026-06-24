9412bc4506e90affc5d0a7a70dfa7fbd267a04f5
saurbhcoder <saurbhcoder@gmail.com>
2026-06-24 10:43:14 +0530

    x

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Channel Stats')
 
 @section('content')
-
+<x-faq-schema :faqs="config('faq.terms-of-use')" />
 <section class="page-content">
   <h1>Terms of Use</h1>
   <p>By using YYTuber, you agree not to misuse the service. This platform is for informational and educational purposes only.</p>
