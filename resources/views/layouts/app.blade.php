d58870a8e4cbc2d8761fd1b5c8a07853908836c8
saurbhcoder <saurbhcoder@gmail.com>
2026-06-25 14:02:13 +0530

    x

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <title>YYTuber Checker</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
-    <link rel="canonical" href="{{ url()->current() }}">ent())">
+    <link rel="canonical" href="{{ url()->current() }}">
     {{-- <script type="application/ld+json">
     {
       "@context":"https://schema.org",
